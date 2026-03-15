d2eb3b6e03088e27979b125960d7a0e0f5a84792
michaelshelden811 <michaelshelden811@gmail.com>
2026-03-15 15:30:25 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,16 @@
 import BillingUX from "../components/BillingUX";
 
 export default function Page() {
-  return <BillingUX />;
+  return (
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "100vw",
+        overflowX: "hidden",
+        padding: "10px"
+      }}
+    >
+      <BillingUX />
+    </div>
+  );
 }
